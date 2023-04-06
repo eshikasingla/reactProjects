7bb6a8588773c0cdbd2d962ecf58cdc69e2e7751
eshikasingla <eshikasingla@gmail.com>
2023-04-06 15:33:42 +0530

    initial

---
+++ src/component/Tasks.js
@@ -0,0 +1,13 @@
+import { useState } from "react"
+import { Task } from "./Task"
+export const Tasks = ({ tasks }) => {
+ return (
+ <>
+ {tasks.map((task) => (<Task key={task.id} task={task} />))}
+ </>
+ )
+}
+
+
+
+  
